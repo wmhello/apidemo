6959816c2eddc270b777acb2a186072734ce0c8e
wmhello <871228582@qq.com>
2018-05-12 09:36:40 +0800

    自动部署测试

--- get_apidemo.php
+++ get_apidemo.php
@@ -36,7 +36,7 @@ $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
 if ($signature) {
   $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
   if (strcmp($signature, $hash) == 0) {
-    echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+    echo shell_exec("cd {$path} && git pull 2>&1");
     exit();
   }
 }
