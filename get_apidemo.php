da311dba3ffa8030b4aaa5f3d5b870d2e35698c1
wmhello <871228582@qq.com>
2018-05-12 09:33:07 +0800

    自动部署

--- get_apidemo.php
+++ get_apidemo.php
@@ -24,4 +24,21 @@
   $command = 'cd ' . str_replace('\\', '/\\', $pwd) . ' && cd .. && cd apidemo && git pull 2>&1';
 
   echo shell_exec($command);
+
+  // GitHub Webhook Secret.
+// Keep it the same with the 'Secret' field on your Webhooks / Manage webhook page of your respostory.
+$secret = "";
+// Path to your respostory on your server.
+// e.g. "/var/www/respostory"
+$path = "";
+// Headers deliveried from GitHub
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+if ($signature) {
+  $hash = "sha1=".hash_hmac('sha1', file_get_contents("php://input"), $secret);
+  if (strcmp($signature, $hash) == 0) {
+    echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+    exit();
+  }
+}
+http_response_code(404);
 ?>
